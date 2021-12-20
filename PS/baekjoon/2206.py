0f647724c0897e9e5a3c40039b07e76461b867ce
dongyeopca <ckehdduq95@gmail.com>
2021-12-20 16:06:00 +0900

    2021.12.20

--- PS/baekjoon/2206.py
+++ PS/baekjoon/2206.py
@@ -3,28 +3,33 @@ from collections import deque
 
 #2트 =>3차원 배열
 
-# dx = [1,-1,0,0]
-# dy = [0,0,1,-1]
-# n,m = map(int,input().split())
-# a = [list(map(int,input())) for _ in range(n)]
-# c = [[[-1]*2 for _ in range(m)] for _ in range(n)]
-# q = deque()
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+n,m = map(int,input().split())
+a = [list(map(int,input())) for _ in range(n)]
+q = deque()
 
-# def bfs():
-#     q.append([0,0,0])
-#     c[0][0][0] = 1
-#     while q:
-#         x,y,z = q.popleft()
-#         for i in range(4):
-#             nx = x + dx[i]
-#             ny = y + dy[i]
-#             if 0 <= nx < n and 0 <= ny < m:
-#                 if a[nx][ny] == 0 and c[nx][ny][z] == -1:
-#                     c[nx][ny][z] = c[x][y][z]+1
-#                     q.append([nx,ny,z])
-#                 elif z == 0 and a[nx][ny] == 1 and c[nx][ny][z+1] == -1:
-#                     c[nx][ny][z+1] = c[]
+def bfs():
+    visit = [[[0]*2 for _ in range(m)] for __ in range(n)]
+    q.append([0,0,1])
+    visit[0][0][1] = 1
+    while q:
+        x,y,w = q.popleft()
+        if x == n -1 and y == m - 1:
+            return visit[x][y][w]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if a[nx][ny] == 1 and w == 1:
+                    visit[nx][ny][0] = visit[x][y][1] +1
+                    q.append([nx,ny,0])
+                elif a[nx][ny] == 0 and visit[nx][ny][w] == 0:
+                    visit[nx][ny][w] = visit[x][y][w] +1
+                    q.append([nx,ny,w])
+    return -1
 
+print(bfs())
 
 
 
